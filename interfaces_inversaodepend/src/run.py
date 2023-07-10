de61a4914262341b1938c12e0b178be498a3b6ae
guimarczewski <guimarczewski@gmail.com>
2023-07-10 23:52:33 +0000

    atualizacao

--- interfaces_inversaodepend/src/run.py
+++ interfaces_inversaodepend/src/run.py
@@ -1,7 +1,8 @@
-from scr.use_case import MyUsecase
-from src.database import Database1
+from src.database import Database2
+from src.use_case import MyUsecase
 
-use_case = MyUsecase(Database1())
+
+use_case = MyUsecase(Database2())
 
 use_case.search_something()
 use_case.insert_something()
