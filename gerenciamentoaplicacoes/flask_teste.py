fcd76675b528929cb365130f5a9e0a7e387ad7bc
guimarczewski <guimarczewski@gmail.com>
2023-07-15 16:23:24 +0000

    refresh gerenciamento

---
+++ gerenciamentoaplicacoes/flask_teste.py
@@ -0,0 +1,11 @@
+#pip3 install -U Flask
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def hello_world():
+    return 'Ola, estou na aplicação'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
